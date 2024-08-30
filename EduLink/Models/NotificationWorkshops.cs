fd3bcb1a78cfad26170c5d581a7a8b4c1fe6ac78
Muath Mhawich <23041194@student.ltuc.com>
2024-08-30 10:39:43 +0300

    add  Entities  in modal folder

---
+++ EduLink/Models/NotificationWorkshops.cs
@@ -0,0 +1,10 @@
+﻿namespace EduLink.Models
+{
+    public class NotificationWorkshops
+    {
+        public int ID { get; set; }
+        public int WorkshopID { get; set; }
+        public DateTime DateSend { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ EduLink/Models/WorkShop.cs
@@ -0,0 +1,14 @@
+﻿namespace EduLink.Models
+{
+    public class WorkShop
+    {
+        public int WorkShopID { get; set; }
+        public int VolunteerID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string SessionLink { get; set; }
+        public int Capasity { get; set; }
+
+    }
+}
