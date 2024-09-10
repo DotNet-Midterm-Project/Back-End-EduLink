a08e99611cd2aff8d6a8eb61455301efa7af538a
Hassan Karraz <24042732@student.ltuc.com>
2024-09-11 02:35:38 +0300

    ADD Forgot and Reset password

---
+++ EduLink/Models/DTO/Request/ForgotPasswordReqDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduLink.Models.DTO.Request
+{
+    public class ForgotPasswordReqDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+
+}
